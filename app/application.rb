8f1ad11fdbe7fa5a64f6d03dd8415450731faba4
Joan Lyness <joanrigdon@gmail.com>
2019-03-27 03:15:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,11 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
+      if @@items.include?(search_term)
+        resp.write "#{search_term} is one of our items"
+      else
+        resp.write "Couldn't find #{search_term}"
+      end
     resp.finish
   end
 
